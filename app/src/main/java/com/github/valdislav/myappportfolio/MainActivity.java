9f5bbbd0145ac64d28e4f9d03af538438c68cba0
Vladyslav Shcherbyna <zpi31-14@acts.kpi.ua>
2015-07-18 22:10:54 +0300

    Toast added

---
+++ app/src/main/java/com/github/valdislav/myappportfolio/MainActivity.java
@@ -0,0 +1,58 @@
+package com.github.valdislav.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the button */
+    public void runApplication(View view) {
+        //TODO: code that will run application will be here
+        if (view instanceof Button) {
+            Toast.makeText(
+                    view.getContext(),
+                    makeStubText(((Button) view).getText()),
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+
+    }
+
+    private CharSequence makeStubText(CharSequence buttonText) {
+        return buttonText.length() > 0 ? "This button will launch " + buttonText : "Error";
+    }
+}
